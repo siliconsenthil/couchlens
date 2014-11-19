8ab2431d4be91c930afa7c72a7b2cde21c273a5e
Senthil V S <vss123@gmail.com>
2014-11-19 08:44:29 +0530

    1) Loading bar added. 2) Fetch button added. 3) Showing error message when connection fails.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,7 +18,8 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'ui.bootstrap'
+    'ui.bootstrap',
+    'angular-loading-bar'
   ])
   .factory('_', ['$window',
     function($window) {
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,13 +15,17 @@ angular.module('lensApp')
       'Karma'
     ];
 
-    $resource('http://localhost:5984/workflow_service_docstore_dev/_all_docs?include_docs=true').get(function(a){
-      $scope.docsByType = _.chain(a.rows).map('doc').groupBy('docType').value();
-      $scope.docTypes = _.without(Object.keys($scope.docsByType), "undefined");
-      $scope.docTypeAttrs = _.reduce($scope.docTypes, function(r, type){r[type] = _.chain($scope.docsByType[type]).map(function(a){
-        return Object.keys(a)}
-      ).flatten().uniq().without("_rev", "docType").value(); return r;}, {})
-      console.log("++++++", $scope.docTypeAttrs);
-    });
+    $scope.fetch = function(){
+      $resource('http://localhost:5984/workflow_service_docstore_dev/_all_docs?include_docs=true').get(function(a){
+        $scope.docsByType = _.chain(a.rows).map('doc').groupBy('docType').value();
+        $scope.docTypes = _.without(Object.keys($scope.docsByType), "undefined");
+        $scope.docTypeAttrs = _.reduce($scope.docTypes, function(r, type){r[type] = _.chain($scope.docsByType[type]).map(function(a){
+            return Object.keys(a)}
+        ).flatten().uniq().without("_rev", "docType").value(); return r;}, {});
+      }, function(x){
+        $scope.showWarning = true;
+      });
+    }
 
+    $scope.fetch();
   });
